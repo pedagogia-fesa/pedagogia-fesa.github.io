276bb407d5da9c913f206f7312a19bfdde1f269f
sebasgguwu <sebaskuhl2003@gmail.com>
2025-05-22 22:59:51 -0600

    Modo obscuro integrado
    Modo obscuro funcional

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ function ajustarAltura() {
 
 ajustarAltura();
 window.addEventListener('resize', ajustarAltura);
+
+// Función modo oscuro
+const botonModoOscuro = document.getElementById('modoOscuroBtn');
+
+botonModoOscuro.addEventListener('click', () => {
+  document.body.classList.toggle('modo-oscuro');
+  
+  // Cambiar icono según el modo
+  if(document.body.classList.contains('modo-oscuro')) {
+    botonModoOscuro.textContent = '₊⋆ ☀︎ ⋆⁺'; // Sol para modo claro
+  } else {
+    botonModoOscuro.textContent = '⏾⋆.˚'; // Luna para modo oscuro
+  }
+});
+
